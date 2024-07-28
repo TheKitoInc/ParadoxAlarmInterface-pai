30a12b80e2d218e4d6b5d11b24ef6051a6440e72
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-07-28 14:52:33 -0300

    Update pai_sync_time.py

--- paradox/console_scripts/pai_sync_time.py
+++ paradox/console_scripts/pai_sync_time.py
@@ -1,66 +1,12 @@
 #!/usr/bin/env python3
 
-import argparse
 import asyncio
 import sys
 
 from paradox.config import config as cfg
 from paradox.lib.encodings import register_encodings
 
-
-def check_version():
-    if sys.version_info < (3, 6,):
-        print(
-            "You are using Python %s.%s, but PAI requires at least Python 3.6"
-            % (sys.version_info[0], sys.version_info[1])
-        )
-        sys.exit(-1)
-
-
-def get_logger():
-    import logging
-    from paradox.main import configure_logger
-    logger = logging.getLogger("PAI").getChild(__name__)
-    configure_logger(logger)
-    return logger
-
-
-def get_alarm():
-    from paradox.paradox import Paradox
-    return Paradox()
-
-
-def init_timezone():
-    import time
-    time.tzset()
-
-
-def get_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-c",
-        "--config",
-        type=str,
-        default=None,
-        help="specify path to an alternative configuration file",
-    )
-    return parser
-
-
-def load_config(args):
-    if "config" in args and args.config is not None:
-        import os
-
-        config_file = os.path.abspath(args.config)
-        cfg.load(config_file)
-    else:
-        cfg.load()
-
-
-def start_interface_manager(alarm):
-    from paradox.interfaces.interface_manager import InterfaceManager
-    interface_manager = InterfaceManager(alarm, config=cfg)
-    interface_manager.start()
+from common import check_version, get_logger, get_alarm, init_timezone, get_parser, load_config, start_interface_manager
 
 
 def get_time_with_timezone():
